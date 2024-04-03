49c3380c6f845ab6aa7fb8112270afcac5cd2dc5
usmanser <165826176+usmanser@users.noreply.github.com>
2024-04-03 13:31:11 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -41,7 +41,7 @@ global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
 global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
+global.autoswview = true //auto status/story view
 global.adminevent = false //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
